532429b2b68d2ff43ea2de3ce2f4faf7be35d75b
livia <livianascimento814@gmail.com>
2023-11-22 22:06:26 -0300

    HomePage atualizada

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -3,10 +3,17 @@ import React from 'react';
 function HomePage() {
   return (
     <div className='bg-gradient-to-b from-cyan-500 to-blue-500 text-black min-h-screen'>
-      <div className='pt-20 pb-10'>
-        <h2>Página Inicial</h2>
-        <p>Bem-vindo à página inicial!</p>
-    </div>
+     <main className='h-screen flex items-center justify-center'>
+      <div className='bg-white flex rounded-lg w-1/2 font-latoRegular overflow-hidden'>
+        <div className='flex-1 text-gray-700 p-20'>
+          <h1 className='text-3xl pb-2 font-latoBold'>Seja Bem-Vindo! 🎉</h1>
+          <p className='text-lg text-gray-500'>Sinta-se à vontade para compartilhar suas experiências na nossa plataforma. 🎮</p>
+        </div>
+        <div className='flex-1 overflow-hidden'>
+           <img src='/video-game.jpeg' className='object-cover w-full h-full rounded-lg' alt='Video Game' />
+         </div>
+       </div>
+      </main>
   </div>
   );
 }
